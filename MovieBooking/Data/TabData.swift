b5a822015281839ed4ea18adc9a793a57b7e9093
Dipak <dipak_pd@outlook.com>
2022-09-17 14:53:35 +0530

    Initial Commit

---
+++ MovieBooking/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  MovieBooking
+//
+//  Created by Codebucket on 17/09/22.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable{
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
